d1afb32d9c36e15c61189718b1fefaead487db15
gautampareek <31121550+gautampareek@users.noreply.github.com>
2025-04-02 16:09:01 +0530

    Update UserUtil.java

--- src/main/java/com/maruj/ticketBooking/Util/UserUtil.java
+++ src/main/java/com/maruj/ticketBooking/Util/UserUtil.java
@@ -3,7 +3,9 @@ package com.maruj.ticketBooking.Util;
 import java.util.Base64;
 
 public class UserUtil {
+    
     public static String getHashedPassword(String password) {
+        System.out.println("___");
         return Base64.getEncoder().encodeToString(password.getBytes());
     }
     public static String setHashedPassword(String hashedPassword) {
